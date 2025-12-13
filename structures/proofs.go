820f66a058ce2e970327f0601f57e3c3ce069748
Vlad <53381472+VladChernenko@users.noreply.github.com>
2025-12-13 02:39:43 +0100

    Remove redundant mempool signature initialization

--- structures/proofs.go
+++ structures/proofs.go
@@ -49,9 +49,75 @@ type AggregatedAnchorRotationProof struct {
 	Signatures map[string]string `json:"signatures"`
 }
 
+func (aarp *AggregatedAnchorRotationProof) UnmarshalJSON(data []byte) error {
+
+	type alias AggregatedAnchorRotationProof
+
+	var aux alias
+
+	if err := json.Unmarshal(data, &aux); err != nil {
+		return err
+	}
+
+	if aux.Signatures == nil {
+		aux.Signatures = make(map[string]string)
+	}
+
+	*aarp = AggregatedAnchorRotationProof(aux)
+
+	return nil
+
+}
+
+func (aarp AggregatedAnchorRotationProof) MarshalJSON() ([]byte, error) {
+
+	type alias AggregatedAnchorRotationProof
+
+	aux := alias(aarp)
+
+	if aux.Signatures == nil {
+		aux.Signatures = make(map[string]string)
+	}
+
+	return json.Marshal(aux)
+}
+
 type AggregatedLeaderFinalizationProof struct {
 	EpochIndex int               `json:"epochIndex"`
 	Leader     string            `json:"leader"`
 	VotingStat VotingStat        `json:"votingStat"`
 	Signatures map[string]string `json:"signatures"`
+}
+
+func (alfp *AggregatedLeaderFinalizationProof) UnmarshalJSON(data []byte) error {
+
+	type alias AggregatedLeaderFinalizationProof
+
+	var aux alias
+
+	if err := json.Unmarshal(data, &aux); err != nil {
+		return err
+	}
+
+	if aux.Signatures == nil {
+		aux.Signatures = make(map[string]string)
+	}
+
+	*alfp = AggregatedLeaderFinalizationProof(aux)
+
+	return nil
+
+}
+
+func (alfp AggregatedLeaderFinalizationProof) MarshalJSON() ([]byte, error) {
+
+	type alias AggregatedLeaderFinalizationProof
+
+	aux := alias(alfp)
+
+	if aux.Signatures == nil {
+		aux.Signatures = make(map[string]string)
+	}
+
+	return json.Marshal(aux)
 }
